82b7ae2fafa3115d227daf0da7b71daa53e5d00a
Corentin Chatillon <corentin.chatillon@etu.utc.fr>
2021-05-02 17:32:59 +0200

    add navbar

---  components/Navigation.tsx
+++  components/Navigation.tsx
@@ -1,24 +1,36 @@
 import Link from "next/link"
-import { Nav } from "rsuite"
+import { Dropdown, Nav, Navbar } from "rsuite"
 
 export function Navigation() {
     return (
-        <Nav>
-            <Nav.Item
-                renderItem={(item) => {
-                    return <Link href="/">{item}</Link>
-                }}
-            >
-                Home
-            </Nav.Item>
+        <>
+            <div style={{ position : "absolute", width : "100%" }}>
+                <img
+                    src=".\Logo.png"
+                    height="100"
+                    style={{ display: "block", margin: "auto" }}
+                />
+            </div>
+            <Navbar appearance="subtle">
+                <Navbar.Body>
+                    <Nav>
+                        <Nav.Item>Accueil</Nav.Item>
+                        <Dropdown title="A propos">
+                            <Dropdown.Item>Company</Dropdown.Item>
+                            <Dropdown.Item>Team</Dropdown.Item>
+                            <Dropdown.Item>Contact</Dropdown.Item>
+                        </Dropdown>
+                        <Nav.Item>Articles</Nav.Item>
+                        <Nav.Item>Nous contacter</Nav.Item>
+                    </Nav>
 
-            <Nav.Item
-                renderItem={(item) => {
-                    return <Link href="/about">{item}</Link>
-                }}
-            >
-                About
-            </Nav.Item>
-        </Nav>
+                    <Nav pullRight>
+                        <Nav.Item>PerfectCorporate</Nav.Item>
+                        <Nav.Item>PerfectCare</Nav.Item>
+                        <Nav.Item>PerfectCircle</Nav.Item>
+                    </Nav>
+                </Navbar.Body>
+            </Navbar>
+        </>
     )
 }
