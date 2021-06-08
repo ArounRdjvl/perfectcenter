fb652e6b542e539bb7da2e585189f5f9b95d3afe
Aroun Radjavelou <aroun.radjavelou@etu.utc.fr>
2021-06-08 18:24:04 +0200

    fix membres

--- pages/membres.tsx
+++ pages/membres.tsx
@@ -12,18 +12,18 @@ const dataMembres = {
         photo: "membres/tom.png",
         poste: "Aucun Poste",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing exercitationem repudiandae nemo iure aperiam!",
-        facebook:"https://www.facebook.com/henrion.tom",
-        instagram:"https://www.instagram.com/tomhenrion/?hl=fr",
-        linkedin:"https://www.linkedin.com/in/tom-henrion-5b7364178/"
+        facebook: "https://www.facebook.com/henrion.tom",
+        instagram: "https://www.instagram.com/tomhenrion/?hl=fr",
+        linkedin: "https://www.linkedin.com/in/tom-henrion-5b7364178/"
     },
     membre2: {
         name: "Aroun Radjavelou",
         photo: "membres/aroun.png",
         poste: "Aucun Poste",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing exercitationem repudiandae nemo iure aperiam!",
-        facebook:"https://www.facebook.com/aroun.rdj.rdj",
-        instagram:"https://www.instagram.com/aroun.rdj/?hl=fr",
-        linkedin:"https://www.linkedin.com/in/aroun-radjavelou-b25a10160/"
+        facebook: "https://www.facebook.com/aroun.rdj.rdj",
+        instagram: "https://www.instagram.com/aroun.rdj/?hl=fr",
+        linkedin: "https://www.linkedin.com/in/aroun-radjavelou-b25a10160/"
     }
 }
 
@@ -37,8 +37,8 @@ export default function membres() {
                 <Navigation />
                 <Membre pair={true} user={dataMembres.membre1} />
                 <Membre pair={false} user={dataMembres.membre2} />
+                <Footer />
             </Container>
-            <Footer/>
         </div>
     )
 }
