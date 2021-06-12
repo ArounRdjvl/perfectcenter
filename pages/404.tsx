f74986dcdb7a66543928cb76a935416b9c324cac
Aroun Radjavelou <aroun.radjavelou@etu.utc.fr>
2021-06-12 09:39:22 +0200

    Update Logo404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,17 +1,46 @@
-import { Button, Container, Link, Typography } from "@material-ui/core";
-import React from "react";
+import React from "react"
+import { Button, Container, Link, Typography } from "@material-ui/core"
+import animation from "../assets/logo404.json"
+import Lottie from "react-lottie"
 
 export default function Home() {
-    return <Container style={{ display: "flex", height: "100%" }}>
-        <div style={{ margin: "auto", display:"flex", flexDirection:"column", alignItems:"center" }}>
-            <img src="Logo.png" alt="Logo PerfecCenter" width="250" />
-            <Typography align="center" variant="h1">
-                Error 404 : Stay Calm, Everything's good.
-            </Typography>
-            
-            <Link href="/" style={{marginTop:"1rem"}}>
-                <Button variant="contained" color="secondary" size="large">Accueil</Button>
-            </Link>
-        </div>
-    </Container>
+    const bodymovinOptions = {
+        loop: true,
+        autoplay: true,
+        prerender: true,
+        animationData: animation
+    }
+
+    return (
+        <Container style={{ display: "flex", height: "100%" }}>
+            <div
+                style={{
+                    margin: "auto",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center"
+                }}
+            >
+                <div
+                    style={{
+                        width: 400
+                    }}
+                >
+                    <Lottie
+                        options={bodymovinOptions}
+                        style={{ margin: "auto" }}
+                    />
+                </div>
+                <Typography align="center" variant="h1">
+                    Error 404 : Stay Calm, Everything's good.
+                </Typography>
+
+                <Link href="/" style={{ marginTop: "1rem" }}>
+                    <Button variant="contained" color="secondary" size="large">
+                        Accueil
+                    </Button>
+                </Link>
+            </div>
+        </Container>
+    )
 }
