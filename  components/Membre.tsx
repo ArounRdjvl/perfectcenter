3b610f7f63dd4be7f32210b39f52fc51182ed267
Aroun Radjavelou <aroun.radjavelou@etu.utc.fr>
2021-06-24 22:14:35 +0200

    add responsive to members page

---  components/Membre.tsx
+++  components/Membre.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Grid, useTheme, Typography } from "@material-ui/core"
+import { Grid, useTheme, Typography, useMediaQuery } from "@material-ui/core"
 import { Divider } from "./Divider"
 
 export interface UserProps {
@@ -20,7 +20,289 @@ export interface MembreProps {
 export function Membre(props: MembreProps) {
     const theme = useTheme()
     const pair = props.pair
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"))
+    if (isMobile) {
+        if (pair) {
+            return (
+                <>
+                    <Grid
+                        justify="center"
+                        direction="column"
+                        container
+                        style={{
+                            maxHeight: "40rem",
+                            marginTop: "3rem"
+                        }}
+                    >
+                        <Grid
+                            xs={12}
+                            style={{
+                                position: "relative",
+                                zIndex: 3
+                            }}
+                            item
+                        >
+                            <div
+                                style={{
+                                    margin: "0 auto",
+                                    display: "flex",
+                                    flexDirection: "row",
+                                    justifyContent: "center",
+                                    backgroundColor: "#E3CCB4",
+                                    borderRadius: "20px",
+                                    height: "12rem",
+                                    width: "16rem"
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        justifyContent: "space-around",
+                                        maxWidth: "40px"
+                                    }}
+                                >
+                                    <a
+                                        href={props.user?.facebook}
+                                        target="_blank"
+                                    >
+                                        <img
+                                            src="picto/Facebook_fonce.png"
+                                            width={40}
+                                            style={{
+                                                zIndex: 2
+                                            }}
+                                        />
+                                    </a>
+                                    <a
+                                        href={props.user?.instagram}
+                                        target="_blank"
+                                    >
+                                        <img
+                                            src="picto/Instagram_fonce.png"
+                                            width={40}
+                                            style={{
+                                                zIndex: 2
+                                            }}
+                                        />
+                                    </a>
+                                    <a
+                                        href={props.user?.linkedin}
+                                        target="_blank"
+                                    >
+                                        <img
+                                            src="picto/Linkedin_fonce.png"
+                                            width={40}
+                                            style={{
+                                                zIndex: 2
+                                            }}
+                                        />
+                                    </a>
+                                </div>
 
+                                <div
+                                    style={{
+                                        position: "relative",
+                                        top: "1rem",
+                                        left: "1rem"
+                                    }}
+                                >
+                                    <img
+                                        style={{
+                                            maxHeight: "10rem",
+                                            borderRadius: "20px"
+                                        }}
+                                        alt={props.user?.name}
+                                        src={props.user?.photo}
+                                    />
+                                </div>
+                            </div>
+                        </Grid>
+                        <Grid
+                            xs={12}
+                            style={{
+                                position: "relative",
+                                minWidth: "16rem"
+                            }}
+                            item
+                        >
+                            <div
+                                style={{
+                                    position: "relative",
+                                    zIndex: 2,
+                                    margin: theme.spacing(2),
+                                    color: "white"
+                                }}
+                            >
+                                <Typography variant="h2" align="center">
+                                    {props.user?.name}
+                                </Typography>
+                                <Divider color="beige" />
+                                <Typography
+                                    variant="h4"
+                                    align="center"
+                                    style={{
+                                        margin: theme.spacing(1)
+                                    }}
+                                >
+                                    {props.user?.poste}
+                                </Typography>
+                                <Typography variant="body1" align="justify">
+                                    {props.user?.desc}
+                                </Typography>
+                            </div>
+                            <div
+                                style={{
+                                    backgroundColor:
+                                        theme.palette.secondary.main,
+                                    position: "absolute",
+                                    height: "100%",
+                                    width: "100%",
+                                    right: 0,
+                                    bottom: 0,
+                                    borderRadius: "20px"
+                                }}
+                            ></div>
+                        </Grid>
+                    </Grid>
+                </>
+            )
+        }
+        return (
+            <>
+                <Grid
+                    justify="center"
+                    container
+                    direction="column"
+                    style={{
+                        maxHeight: "40rem",
+                        marginTop: "3rem"
+                    }}
+                >
+                    <Grid
+                        xs={12}
+                        style={{
+                            position: "relative",
+                            zIndex: 3
+                        }}
+                        item
+                    >
+                        <div
+                            style={{
+                                margin: "0 auto",
+                                display: "flex",
+                                flexDirection: "row",
+                                justifyContent: "center",
+                                backgroundColor: theme.palette.secondary.main,
+                                borderRadius: "20px",
+                                height: "12rem",
+                                width: "16rem"
+                            }}
+                        >
+                            <div
+                                style={{
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    justifyContent: "space-around",
+                                    maxWidth: "40px"
+                                }}
+                            >
+                                <a href={props.user?.facebook} target="_blank">
+                                    <img
+                                        src="picto/Facebook_clair.png"
+                                        width={40}
+                                        style={{
+                                            zIndex: 2
+                                        }}
+                                    />
+                                </a>
+                                <a href={props.user?.instagram} target="_blank">
+                                    <img
+                                        src="picto/Instagram_clair.png"
+                                        width={40}
+                                        style={{
+                                            zIndex: 2
+                                        }}
+                                    />
+                                </a>
+                                <a href={props.user?.linkedin} target="_blank">
+                                    <img
+                                        src="picto/Linkedin_clair.png"
+                                        width={40}
+                                        style={{
+                                            zIndex: 2
+                                        }}
+                                    />
+                                </a>
+                            </div>
+
+                            <div
+                                style={{
+                                    position: "relative",
+                                    top: "1rem",
+                                    left: "1rem"
+                                }}
+                            >
+                                <img
+                                    style={{
+                                        maxHeight: "10rem",
+                                        borderRadius: "20px"
+                                    }}
+                                    alt={props.user?.name}
+                                    src={props.user?.photo}
+                                />
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid
+                        xs={12}
+                        style={{
+                            position: "relative",
+                            minWidth: "16rem"
+                        }}
+                        item
+                    >
+                        <div
+                            style={{
+                                position: "relative",
+                                zIndex: 2,
+                                margin: theme.spacing(2)
+                            }}
+                        >
+                            <Typography variant="h2" align="center">
+                                {props.user?.name}
+                            </Typography>
+                            <Divider color="vert" />
+                            <Typography
+                                variant="h4"
+                                align="center"
+                                style={{
+                                    margin: theme.spacing(1)
+                                }}
+                            >
+                                {props.user?.poste}
+                            </Typography>
+                            <Typography variant="body1" align="justify">
+                                {props.user?.desc}
+                            </Typography>
+                        </div>
+                        <div
+                            style={{
+                                backgroundColor: "#E3CCB4",
+                                position: "absolute",
+                                height: "100%",
+                                width: "100%",
+                                left: 0,
+                                bottom: 0,
+                                borderRadius: "20px"
+                            }}
+                        ></div>
+                    </Grid>
+                    
+                </Grid>
+            </>
+        )
+    }
     if (pair) {
         return (
             <>
@@ -29,8 +311,8 @@ export function Membre(props: MembreProps) {
                     container
                     style={{
                         maxHeight: "40rem",
-                        marginLeft:"-5rem",
-                        marginBottom: "3rem"
+                        marginLeft: "-5rem",
+                        marginTop: "3rem"
                     }}
                 >
                     <Grid
@@ -114,7 +396,7 @@ export function Membre(props: MembreProps) {
                                 zIndex: 2,
                                 margin: theme.spacing(2),
                                 color: "white",
-                                paddingRight:theme.spacing(5)
+                                paddingRight: theme.spacing(5)
                             }}
                         >
                             <Typography variant="h2" align="center">
@@ -157,8 +439,8 @@ export function Membre(props: MembreProps) {
                 container
                 style={{
                     maxHeight: "40rem",
-                    marginLeft:"5rem",
-                    marginBottom: "3rem"
+                    marginLeft: "5rem",
+                    marginTop: "3rem"
                 }}
             >
                 <Grid
@@ -174,7 +456,7 @@ export function Membre(props: MembreProps) {
                             position: "relative",
                             zIndex: 2,
                             margin: theme.spacing(2),
-                            paddingLeft:theme.spacing(5)
+                            paddingLeft: theme.spacing(5)
                         }}
                     >
                         <Typography variant="h2" align="center">
