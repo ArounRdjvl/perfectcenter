b320b3805c3c3833300e3c0ac291b8e54552e133
Corentin Chatillon <corentin.chatillon@etu.utc.fr>
2021-06-24 18:02:42 +0200

    adding menu for mobile devices

---  components/Navigation.tsx
+++  components/Navigation.tsx
@@ -7,15 +7,26 @@ import {
     Fade,
     Container,
     makeStyles,
-    Link
+    Link,
+    useMediaQuery,
+    useTheme,
+    IconButton,
+    Popover,
+    Divider
 } from "@material-ui/core"
-import { FiberPin } from "@material-ui/icons"
+import MenuIcon from "@material-ui/icons/Menu"
+import MenuOpenIcon from "@material-ui/icons/MenuOpen"
+import { animated, config, useSpring } from "react-spring"
 
 export const heightHeader = 90
 
 const useStyles = makeStyles((theme) => ({
     boxShadowNone: {
         boxShadow: "none"
+    },
+    buttonMobile: {
+        width: "100%",
+        height: "100%"
     }
 }))
 
@@ -23,12 +34,19 @@ export interface NavigationProps {
     displayLogo?: boolean
 }
 
+const vhToPixel = (value: number) => (window.innerHeight * value) / 100
+
 export function Navigation(props: NavigationProps) {
     const displayLogo = props.displayLogo != null ? props.displayLogo : true
     const [anchorEl, setAnchorEl] = React.useState(null)
     const [shadow, setShadow] = React.useState<boolean>(false)
 
+    const [menuOpen, setMenuOpen] = React.useState<boolean>(false)
+
+    const theme = useTheme()
     const classes = useStyles()
+
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"))
 
     function handleClick(event: any) {
         setAnchorEl(event.currentTarget)
@@ -53,6 +71,138 @@ export function Navigation(props: NavigationProps) {
             window.removeEventListener("scroll", handleScroll)
         }
     }, [])
+
+    const ref = React.useRef<HTMLDivElement>(null)
+
+    const springProps = useSpring({
+        height: menuOpen ? vhToPixel(100) : heightHeader,
+        config: config.default,
+        onRest: () => {
+            if (menuOpen && ref.current) {
+                ref.current.style.height = "100vh"
+            }
+        }
+    })
+
+    if (isMobile) {
+        return (
+            <>
+                <div style={{ height: heightHeader, width: "100%" }} />
+                <AppBar
+                    color="transparent"
+                    position="fixed"
+                    style={{
+                        minHeight: heightHeader,
+                        backgroundColor: "#EBEAE8",
+                        zIndex: 9
+                    }}
+                    className={!shadow ? classes.boxShadowNone : ""}
+                >
+                    <animated.div
+                        ref={ref}
+                        style={{
+                            height: springProps.height
+                        }}
+                    >
+                        <Container
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                height: "100%",
+                                overflow: "hidden",
+                                justifyContent: "flex-start"
+                            }}
+                        >
+                            <div
+                                style={{
+                                    position: "relative",
+                                    minHeight: "100vh",
+                                    display: "flex",
+                                    flexDirection: "column"
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        height: heightHeader,
+                                        display: "flex"
+                                    }}
+                                >
+                                    <IconButton
+                                        onClick={() => setMenuOpen(!menuOpen)}
+                                    >
+                                        {menuOpen ? (
+                                            <MenuOpenIcon />
+                                        ) : (
+                                            <MenuIcon />
+                                        )}
+                                    </IconButton>
+                                </div>
+                                <div
+                                    style={{
+                                        flexGrow: 1,
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        justifyContent: "center"
+                                    }}
+                                >
+                                    <Link href="/">
+                                        <Button
+                                            className={classes.buttonMobile}
+                                        >
+                                            Accueil
+                                        </Button>
+                                    </Link>
+                                    <Link href="/membres">
+                                        <Button
+                                            className={classes.buttonMobile}
+                                        >
+                                            Membres
+                                        </Button>
+                                    </Link>
+                                    <Link href="/articles">
+                                        <Button
+                                            className={classes.buttonMobile}
+                                        >
+                                            Articles
+                                        </Button>
+                                    </Link>
+                                    <Link href="/contact">
+                                        <Button
+                                            className={classes.buttonMobile}
+                                        >
+                                            Nous contacter
+                                        </Button>
+                                    </Link>
+                                    <Divider />
+                                    <Link href="/perfectcorporate">
+                                        <Button
+                                            className={classes.buttonMobile}
+                                        >
+                                            PerfectCorporate
+                                        </Button>
+                                    </Link>
+                                    <Link href="/perfectcare">
+                                        <Button
+                                            className={classes.buttonMobile}
+                                        >
+                                            PerfectCare
+                                        </Button>
+                                    </Link>
+                                    <Link href="/perfectcircle">
+                                        <Button
+                                            className={classes.buttonMobile}
+                                        >
+                                            PerfectCircle
+                                        </Button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </Container>
+                    </animated.div>
+                </AppBar>
+            </>
+        )
+    }
 
     return (
         <>
