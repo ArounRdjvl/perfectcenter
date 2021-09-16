3323f2d038205ab8d76bc9a2081df4775cc679ca
Corentin Chatillon <corentin.chatillon@etu.utc.fr>
2021-09-16 09:59:40 +0200

    adding https

--- server.js
+++ server.js
@@ -2,25 +2,38 @@ const { createServer } = require('https');
 const { parse } = require('url');
 const next = require('next');
 const fs = require('fs');
+const express = require("express");
+
+const httpRedirectServer = express();
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const httpsOptions = {
-    key: fs.readFileSync('./certificates/localhost.key'),
-    cert: fs.readFileSync('./certificates/localhost.crt')
+    key: fs.readFileSync('./certificates/key.pem'),
+    cert: fs.readFileSync('./certificates/cert.pem')
 };
+
+// set up a route to redirect http to https
+httpRedirectServer.get('*', (req, res) => {
+    res.redirect('https://' + req.headers.host + req.url);
+});
+
+httpRedirectServer.listen(80);
 
 app.prepare().then(() => {
     createServer(httpsOptions, (req, res) => {
         // TOFIX : deprecated url.parse
+        // const baseURL =  'https://' + req.headers.host + '/';
+        // const reqUrl = new URL(req.url,baseURL);
+        
         const parsedUrl = parse(req.url, true);
-        // const baseURL = req.protocol || "http" + '://' + req.headers.host + '/';
+
         handle(req, res, parsedUrl);
 
-    }).listen(3000, err => {
+    }).listen(443, err => {
         if (err) throw err;
-        console.log('> Ready on https://localhost:3000');
+        console.log('> Ready on https://localhost:443');
     });
 });
