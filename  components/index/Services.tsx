c5315f8dde857864f2f47684afc8104b84c8b16e
Aroun Radjavelou <aroun.radjavelou@etu.utc.fr>
2021-10-02 17:16:36 +0200

    add margin on several div to put arrow inside green div

---  components/index/Services.tsx
+++  components/index/Services.tsx
@@ -19,10 +19,7 @@ export function Services() {
 
     return (
         <>
-            <Typography
-                variant="h1"
-                align="center"
-            >
+            <Typography variant="h1" align="center">
                 Nos Services
             </Typography>
             <Divider color="vert" />
@@ -37,10 +34,11 @@ export function Services() {
                             style={{ margin: "auto" }}
                         />
                     </div>
-                    <Typography variant="h4" style={{ margin: "1rem" }}>
+                    <Typography variant="h4" style={{ marginTop: "1rem" }}>
                         Nos Praticiens
                     </Typography>
                     <IconButton
+                        style={{ marginTop: "1.5rem" }}
                         className={classes.buttonStyles}
                         onClick={() => setClicked("circle")}
                     >
@@ -69,6 +67,7 @@ export function Services() {
                         Nos produits
                     </Typography>
                     <IconButton
+                        style={{ marginTop: "1.5rem" }}
                         className={classes.buttonStyles}
                         onClick={() => setClicked("care")}
                     >
@@ -92,6 +91,7 @@ export function Services() {
                         Entreprise
                     </Typography>
                     <IconButton
+                        style={{ marginTop: "1.5rem" }}
                         className={classes.buttonStyles}
                         onClick={() => setClicked("corporate")}
                     >
